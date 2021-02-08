eeb0a8eaf44695a50ec291e2a4324513824baaff
laur80 <laur80raul@gmail.com>
2021-02-08 16:00:14 +0200

    up

--- compo/Navbar.tsx
+++ compo/Navbar.tsx
@@ -6,18 +6,18 @@ const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <h4 className="navbar-brand" >Recipes search</h4>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <Link href="/" ><a className="navbar-brand" >Recipes search</a></Link>
+      {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
           <Link href="/">
-          <a className="nav-link active" aria-current="page" >Home</a>
+          <a className="nav-link active" aria-current="page" >Recipes search</a>
           </Link>
-          {/* <a className="nav-link" href="#">Features</a> */}
+          <a className="nav-link" href="#">Features</a>
         </div>
-      </div>
+      </div> */}
     </div>
   </nav>
   );
